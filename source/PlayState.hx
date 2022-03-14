5f83bb9ea080433752be809b5aaa5d1bc4891b32
Josenene8 <100051827+Josenene8@users.noreply.github.com>
2022-03-13 22:05:02 -0300

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,23 +1,41 @@
+
 package;
 
+import flixel.input.keyboard.FlxKey;
+import haxe.Exception;
+import openfl.geom.Matrix;
+import openfl.display.BitmapData;
+import openfl.utils.AssetType;
+import lime.graphics.Image;
+import flixel.graphics.FlxGraphic;
+import openfl.utils.AssetManifest;
+import openfl.utils.AssetLibrary;
+import flixel.system.FlxAssets;
 
-import ChromaticAberrationShader;
-import Note.NoteType;
-import Replay.Ana;
-import Replay.Analysis;
+import lime.app.Application;
+import lime.media.AudioContext;
+import lime.media.AudioManager;
+import openfl.Lib;
 import Section.SwagSection;
 import Song.SwagSong;
+import WiggleEffect.WiggleEffectType;
 import flixel.FlxBasic;
 import flixel.FlxCamera;
 import flixel.FlxG;
+import flixel.FlxGame;
 import flixel.FlxObject;
 import flixel.FlxSprite;
+import flixel.FlxState;
 import flixel.FlxSubState;
-import flixel.addons.text.FlxTypeText;
+import flixel.addons.display.FlxGridOverlay;
+import flixel.addons.effects.FlxTrail;
+import flixel.addons.effects.FlxTrailArea;
+import flixel.addons.effects.chainable.FlxEffectSprite;
+import flixel.addons.effects.chainable.FlxWaveEffect;
 import flixel.addons.transition.FlxTransitionableState;
-import flixel.effects.particles.FlxEmitter;
+import flixel.graphics.atlas.FlxAtlas;
+import flixel.graphics.frames.FlxAtlasFrames;
 import flixel.group.FlxGroup.FlxTypedGroup;
-import flixel.input.keyboard.FlxKey;
 import flixel.math.FlxMath;
 import flixel.math.FlxPoint;
 import flixel.math.FlxRect;
@@ -26,15 +44,23 @@ import flixel.text.FlxText;
 import flixel.tweens.FlxEase;
 import flixel.tweens.FlxTween;
 import flixel.ui.FlxBar;
+import flixel.util.FlxCollision;
 import flixel.util.FlxColor;
 import flixel.util.FlxSort;
+import flixel.util.FlxStringUtil;
 import flixel.util.FlxTimer;
-import lime.app.Application;
-import openfl.Lib;
+import haxe.Json;
+import lime.utils.Assets;
 import openfl.display.BlendMode;
-import openfl.events.KeyboardEvent;
-import openfl.media.Sound;
-import openfl.ui.Keyboard;
+import openfl.display.StageQuality;
+import openfl.filters.ShaderFilter;
+import Sys;
+import sys.FileSystem;
+import openfl.Assets;
+import sys.io.File;
+#if mobileC
+import ui.Mobilecontrols;
+#end
 
 using StringTools;
 #if sys
