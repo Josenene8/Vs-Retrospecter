bb14b2e15dfa4b88f192f0d536b37c42c2e6adea
Josenene8 <100051827+Josenene8@users.noreply.github.com>
2022-03-15 07:33:20 -0300

    Add files via upload

--- source/KadeEngineData.hx
+++ source/KadeEngineData.hx
@@ -1,22 +1,95 @@
-import flixel.FlxG;
 import flixel.input.gamepad.FlxGamepad;
 import openfl.Lib;
+import flixel.FlxG;
+#if mobileC
+import flixel.util.FlxSave;
+import mobile.FlxVirtualPad;
+import flixel.math.FlxPoint;
+#end
 
 class KadeEngineData
 {
-    public static function initSave()
+	#if mobileC
+	// its from config file
+	var save:FlxSave;
+
+    public function new() 
     {
-        //if (FlxG.save.data.weekUnlocked == null)
-		//	FlxG.save.data.weekUnlocked = 1;
+        save = new FlxSave();
+    	save.bind("saveconrtol");
+    }
+
+	public function getcontrolmode():Int {
+        // load control mode num from FlxSave
+		if (save.data.buttonsmode != null) return save.data.buttonsmode[0];
+        return 0;
+    }
+
+    public function setcontrolmode(mode:Int = 0):Int {
+        // save control mode num from FlxSave
+		if (save.data.buttonsmode == null) save.data.buttonsmode = new Array();
+        save.data.buttonsmode[0] = mode;
+        save.flush();
+
+        return save.data.buttonsmode[0];
+    }
+
+    public function savecustom(_pad:FlxVirtualPad) {
+		trace("saved");
+
+		if (save.data.buttons == null)
+		{
+			save.data.buttons = new Array();
+
+			for (buttons in _pad)
+			{
+				save.data.buttons.push(FlxPoint.get(buttons.x, buttons.y));
+			}
+		}else
+		{
+			var tempCount:Int = 0;
+			for (buttons in _pad)
+			{
+				save.data.buttons[tempCount] = FlxPoint.get(buttons.x, buttons.y);
+				tempCount++;
+			}
+		}
+		save.flush();
+	}
+
+	public function loadcustom(_pad:FlxVirtualPad):FlxVirtualPad {
+		//load pad
+		if (save.data.buttons == null) return _pad;
+		var tempCount:Int = 0;
+
+		for(buttons in _pad)
+		{
+			buttons.x = save.data.buttons[tempCount].x;
+			buttons.y = save.data.buttons[tempCount].y;
+			tempCount++;
+		}	
+        return _pad;
+	}
+	#end
+
+	public static function initSave()
+	{
+        if (FlxG.save.data.newInput == null)
+			FlxG.save.data.newInput = true;
 
 		if (FlxG.save.data.downscroll == null)
 			FlxG.save.data.downscroll = false;
 
-		if (FlxG.save.data.antialiasing == null)
-			FlxG.save.data.antialiasing = true;
+		if (FlxG.save.data.middlescroll == null)
+			FlxG.save.data.middlescroll = false;
 
-		if (FlxG.save.data.missSounds == null)
-			FlxG.save.data.missSounds = true;
+		if (FlxG.save.data.dfjk == null)
+			FlxG.save.data.dfjk = false;
+
+		#if mobileC
+		if (FlxG.save.data.fastValue == null)
+			FlxG.save.data.fastValue = 0;
+		#end
 
 		if (FlxG.save.data.accuracyDisplay == null)
 			FlxG.save.data.accuracyDisplay = true;
@@ -27,8 +100,10 @@ class KadeEngineData
 		if (FlxG.save.data.songPosition == null)
 			FlxG.save.data.songPosition = false;
 
-		if (FlxG.save.data.fps == null)
-			FlxG.save.data.fps = false;
+		#if desktop
+		if (FlxG.save.data.inputShow == null)
+			FlxG.save.data.inputShow = false;
+		#end
 
 		if (FlxG.save.data.changedHit == null)
 		{
@@ -37,14 +112,26 @@ class KadeEngineData
 			FlxG.save.data.changedHit = false;
 		}
 
-		if (FlxG.save.data.fpsRain == null)
-			FlxG.save.data.fpsRain = false;
+		#if mobileC
+		if (FlxG.save.data.fpsCap == null)
+			FlxG.save.data.fpsCap = 60;
+
+		if (FlxG.save.data.fpsCap > 90)
+			FlxG.save.data.fpsCap = 60; // baby proof so you can't hard lock ur copy of kade engine
+		#else
 
 		if (FlxG.save.data.fpsCap == null)
 			FlxG.save.data.fpsCap = 120;
 
 		if (FlxG.save.data.fpsCap > 285 || FlxG.save.data.fpsCap < 60)
 			FlxG.save.data.fpsCap = 120; // baby proof so you can't hard lock ur copy of kade engine
+		#end
+
+		if (FlxG.save.data.fps == null)
+			FlxG.save.data.fps = false;
+
+		if (FlxG.save.data.fpsRain == null)
+			FlxG.save.data.fpsRain = false;
 
 		if (FlxG.save.data.scrollSpeed == null)
 			FlxG.save.data.scrollSpeed = 1;
@@ -64,53 +151,26 @@ class KadeEngineData
 		if (FlxG.save.data.ghost == null)
 			FlxG.save.data.ghost = true;
 
-		if (FlxG.save.data.background == null)
-			FlxG.save.data.background = 2;
-
-		if (FlxG.save.data.modChart == null)
-			FlxG.save.data.modChart = true;
-
-		if (FlxG.save.data.stepMania == null)
-			FlxG.save.data.stepMania = false;
+		if (FlxG.save.data.distractions == null)
+			FlxG.save.data.distractions = true;
 
 		if (FlxG.save.data.flashing == null)
 			FlxG.save.data.flashing = true;
 
-		if (FlxG.save.data.motion == null)
-			FlxG.save.data.motion = true;
-
-		if (FlxG.save.data.chrom == null)
-			FlxG.save.data.chrom = true;
-
-		if (FlxG.save.data.ghostTrails == null)
-			FlxG.save.data.ghostTrails = true;
-
-		if (FlxG.save.data.particles == null)
-			FlxG.save.data.particles = true;
-
-		if (FlxG.save.data.screenShake == null)
-			FlxG.save.data.screenShake = true;
-
-		if (FlxG.save.data.windowShake == null)
-			FlxG.save.data.windowShake = true;
-
 		if (FlxG.save.data.resetButton == null)
 			FlxG.save.data.resetButton = false;
-
-		if (FlxG.save.data.InstantRespawn == null)
-			FlxG.save.data.InstantRespawn = false;
 
 		if (FlxG.save.data.botplay == null)
 			FlxG.save.data.botplay = false;
 
 		if (FlxG.save.data.cpuStrums == null)
-			FlxG.save.data.cpuStrums = true;
+			FlxG.save.data.cpuStrums = false;
 
 		if (FlxG.save.data.strumline == null)
 			FlxG.save.data.strumline = false;
 
 		if (FlxG.save.data.customStrumLine == null)
-			FlxG.save.data.customStrumLine = true;
+			FlxG.save.data.customStrumLine = 0;
 
 		if (FlxG.save.data.camzoom == null)
 			FlxG.save.data.camzoom = true;
@@ -121,80 +181,21 @@ class KadeEngineData
 		if (FlxG.save.data.inputShow == null)
 			FlxG.save.data.inputShow = false;
 
-		if (FlxG.save.data.optimize == null)
-			FlxG.save.data.optimize = false;
+		/*if (FlxG.save.data.optimize == null)
+			FlxG.save.data.optimize = false;*/
 
-		if (FlxG.save.data.cacheImages == null)
-			FlxG.save.data.cacheImages = true;
+		if (FlxG.save.data.char == null)
+			FlxG.save.data.char = true;
 
-		if (FlxG.save.data.cacheCutscenes == null)
-			FlxG.save.data.cacheCutscenes = true;
+		if (FlxG.save.data.bg == null)
+			FlxG.save.data.bg = true;
 
-		if (FlxG.save.data.editor == null)
-			FlxG.save.data.editor = true;
-
-		if (FlxG.save.data.zoom == null)
-			FlxG.save.data.zoom = 1;
+		if (FlxG.save.data.freeplayMusic == null)
+			FlxG.save.data.freeplayMusic = false;
 
 		var gamepad:FlxGamepad = FlxG.gamepads.lastActive;
 
 		KeyBinds.gamepad = gamepad != null;
-
-		// Data for Retro mod
-		// Load unlocked mode data
-		if (FlxG.save.data.modeUnlocked != null)
-		{
-			for (i in 0...StoryMenuState.modeUnlocked.length)
-			{
-				// (Arcy) Fail-safe for data not being the same length
-				if (FlxG.save.data.modeUnlocked[i] != null)
-					StoryMenuState.modeUnlocked[i] = FlxG.save.data.modeUnlocked[i];
-			}
-		}
-
-		// Load unlocked character data
-		if (FlxG.save.data.characterUnlocked != null)
-		{
-			for (i in 0...StoryMenuState.characterUnlocked.length)
-			{
-				// (Arcy) Fail-safe for data not being the same length
-				if (FlxG.save.data.characterUnlocked[i] != null)
-					StoryMenuState.characterUnlocked[i] = FlxG.save.data.characterUnlocked[i];
-			}
-		}
-
-		// Load unlocked girlfriend data
-		if (FlxG.save.data.girlfriendUnlocked != null)
-		{
-			for (i in 0...StoryMenuState.girlfriendUnlocked.length)
-			{
-				// (Arcy) Fail-safe for data not being the same length
-				if (FlxG.save.data.girlfriendUnlocked[i] != null)
-					StoryMenuState.girlfriendUnlocked[i] = FlxG.save.data.girlfriendUnlocked[i];
-			}
-		}
-
-		// Load unlocked song data
-		if (FlxG.save.data.songUnlocked != null)
-		{
-			for (i in 0...FreeplayState.songUnlocked.length)
-			{
-				// (Arcy) Fail-safe for data not being the same length
-				if (FlxG.save.data.songUnlocked[i] != null)
-					FreeplayState.songUnlocked[i] = FlxG.save.data.songUnlocked[i];
-			}
-		}
-
-		// Load visible song data
-		if (FlxG.save.data.songVisible != null)
-		{
-			for (i in 0...FreeplayState.songVisible.length)
-			{
-				// (Arcy) Fail-safe for data not being the same length
-				if (FlxG.save.data.songVisible[i] != null)
-					FreeplayState.songVisible[i] = FlxG.save.data.songVisible[i];
-			}
-		}
 
 		Conductor.recalculateTimings();
 		PlayerSettings.player1.controls.loadKeyBinds();
